25bda8aed963e70f0a21006b646c6584c02ef39a
AmeeraMOhammed <87406476+AmeeraMOhammed@users.noreply.github.com>
2023-12-12 02:28:20 +0200

    login page

--- src/Screens/login.js
+++ src/Screens/login.js
@@ -1,170 +1,52 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
-import {Input, Button, Icon} from 'react-native-elements';
-// import {FontAwesome5} from 'react-native-vector-icons';
-import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native';
 
-function Login() {
-  const navigation = useNavigation();
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.Middle}>
-        <Text style={styles.LoginText}>Login</Text>
-      </View>
-      <View style={styles.text2}>
-        <Text>Don't have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
-          <Text style={styles.signupText}> Sign up</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* Username or Email Input Field */}
-      <View style={styles.buttonStyle}>
-        <Input
-          leftIcon={
-            <Icon
-              name="user-secret"
-              size={24}
-              color="black"
-              type="font-awesome-5"
-            />
-          }
-          placeholder="Username or Email"
-        />
-      </View>
-
-      {/* Password Input Field */}
-      <View style={styles.buttonStyleX}>
-        <Input
-          leftIcon={
-            <Icon name="key" size={24} color="black" type="font-awesome-5" />
-          }
-          secureTextEntry={true}
-          placeholder="Password"
-        />
-      </View>
-
-      {/* Button */}
-      <View style={styles.buttonStyle}>
-        <Button
-          title="LOGIN"
-          buttonStyle={styles.buttonDesign}
-          onPress={() => {}}
-        />
-      </View>
-
-      {/* Line */}
-      <View style={styles.lineStyle}>
-        <View style={styles.lineText} />
-        <View>
-          <Text style={styles.text_4}>Or</Text>
-        </View>
-        <View style={styles.viewStyle} />
-      </View>
-
-      {/* Image */}
-      <View style={styles.boxStyle}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('#')}
-          style={styles.imageStyle}>
-          <Image
-            style={styles.imageSource}
-            source={{
-              uri: 'https://www.transparentpng.com/thumb/google-logo/colorful-google-logo-transparent-clipart-download-u3DWLj.png',
-            }}
-          />
-        </TouchableOpacity>
-        {/* Add similar TouchableOpacity components for other images */}
-      </View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
-}
-
-export default function App() {
-  return <Login />;
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  LoginText: {
-    marginTop: 100,
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  Middle: {
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-  viewStyle: {
-    flex: 1,
-    height: 1,
-    backgroundColor: 'black',
-  },
-  text_4: {
-    width: 50,
-    textAlign: 'center',
-  },
-  text2: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    paddingTop: 5,
-  },
-  signupText: {
-    fontWeight: 'bold',
-  },
-  emailField: {
-    marginTop: 30,
-    marginLeft: 15,
-  },
-  emailInput: {
-    marginTop: 10,
-    marginRight: 5,
-  },
-  lineText: {
-    flex: 1,
-    height: 1,
-    backgroundColor: 'black',
-  },
-  buttonStyle: {
-    marginTop: 30,
-    marginLeft: 15,
-    marginRight: 15,
-  },
-  buttonStyleX: {
-    marginTop: 12,
-    marginLeft: 15,
-    marginRight: 15,
-  },
-  buttonDesign: {
-    backgroundColor: '#026efd',
-  },
-  lineStyle: {
-    flexDirection: 'row',
-    marginTop: 30,
-    marginLeft: 15,
-    marginRight: 15,
     alignItems: 'center',
+    padding: 16,
   },
-  imageStyle: {
-    width: 80,
-    height: 80,
-    marginLeft: 20,
-  },
-  imageSource: {
-    width: 80,
-    height: 80,
-  },
-
-  boxStyle: {
-    flexDirection: 'row',
-    marginTop: 30,
-    marginLeft: 15,
-    marginRight: 15,
-    justifyContent: 'space-around',
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 10,
   },
 });
+
+export default LoginScreen;
